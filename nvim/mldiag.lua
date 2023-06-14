dd1442b3743919b30a16a4fbe29cdd330818b8aa
marios <haha@myzen.jaist.ac.jp>
2023-06-14 10:12:26 +0900

    Use lua print to avoid string escaping

--- nvim/mldiag.lua
+++ nvim/mldiag.lua
@@ -2,30 +2,19 @@ local exists_previous_message = false
 
 local get_current_line_diagnostic = function()
   local cur = vim.api.nvim_win_get_cursor(0)
-  return vim.diagnostic.get(
-    0, { lnum = cur[1] - 1
-    })[1]
+  return vim.diagnostic.get(0, { lnum = cur[1] - 1 })[1]
 end
 
--- To avoid hit-enter-prompts, truncate the message
+-- To avoid hit-enter-prompts, first treat whitespace and truncate the message
 local truncate = function(msg)
-  return
-      string.sub(
-        string.gsub(msg, "[\n\r\t]", ". "),
-        1,
-        vim.v.echospace)
+  return msg:gsub("[\n\r\t]", ". "):sub(1, vim.v.echospace)
 end
 
 local echo_diagnostics = function()
   local first_line_diag = get_current_line_diagnostic()
 
   if first_line_diag ~= nil then
-    local truncated_diagnostic = truncate(first_line_diag.message)
-    local strcmd = string.format(
-      "echo '%s'",
-      truncated_diagnostic)
-
-    vim.api.nvim_command(strcmd)
+    print(truncate(first_line_diag.message))
     exists_previous_message = true
   elseif exists_previous_message == true then
     vim.api.nvim_command('echo ')
